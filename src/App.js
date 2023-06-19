516ca3f6062ac496d61c0543caa4cc5de1240c0c
ramsai24 <jnrsai24@gmail.com>
2023-06-19 07:33:39 +0000

    class completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import Clock from './components/Clock'
 import './App.css'
 
 class App extends Component {
+  state = {showClock: false}
+
+  //   onToggleClock = () => this.setState(prev => ({showClock: !prev.showClock}))
+  onToggleClock = () => {
+    this.setState(pre => {
+      const {showClock} = pre
+      return {
+        showClock: !showClock,
+      }
+    })
+  }
+
   render() {
+    const {showClock} = this.state
+
     return (
       <div className="app-container">
-        <button type="button" className="toggle-button">
-          Hide Clock
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.onToggleClock}
+        >
+          {showClock ? 'Hide Clock' : 'Show Clock'}
         </button>
-        <Clock />
+        {showClock && <Clock />}
       </div>
     )
   }
--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,10 +2,33 @@ import {Component} from 'react'
 import './index.css'
 
 class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('Constructor called')
+  }
+
+  componentDidMount() {
+    console.log('ComponentDidMount Called')
+    this.timeID = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount called')
+    clearInterval(this.timerID)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
   render() {
+    const {date} = this.state
+    console.log('Render Called', date)
     return (
       <div className="clock-container">
         <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
       </div>
     )
   }
